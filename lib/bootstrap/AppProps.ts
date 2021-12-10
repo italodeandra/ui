4f2c3fb69a94d435bafc42c48ca5067351584736
Ítalo Andrade <italodeandra@gmail.com>
2021-12-10 17:16:13 +0100

    add types

--- lib/bootstrap/AppProps.ts
+++ lib/bootstrap/AppProps.ts
@@ -1,6 +1,10 @@
 import type { EmotionCache } from "@emotion/cache";
 import type { AppProps as NextAppProps } from "next/app";
+import GetLayout from "./GetLayout";
 
-type AppProps = NextAppProps & { emotionCache: EmotionCache };
+type AppProps = NextAppProps & {
+  emotionCache: EmotionCache;
+  Component: NextAppProps["Component"] & GetLayout;
+};
 
 export default AppProps;
---
+++ lib/bootstrap/GetLayout.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from "react";
+
+type GetLayout = {
+  getLayout: (c: ReactNode) => ReactNode;
+};
+
+export default GetLayout;
