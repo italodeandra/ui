f6388f19ae8ec700b020f6f3726836a7b9aea1b5
Ítalo Andrade <italodeandra@gmail.com>
2022-10-27 18:17:44 -0300

    improve breadcrumbs

--- lib/components/Breadcrumbs.tsx
+++ lib/components/Breadcrumbs.tsx
@@ -1,4 +1,11 @@
-import { Anchor, Box, Paper, Text, useMantineTheme } from "@mantine/core";
+import {
+  Anchor,
+  Box,
+  MediaQuery,
+  Paper,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 import { IconHome } from "@tabler/icons";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
@@ -6,6 +13,7 @@ import { ReactNode } from "react";
 
 export interface IBreadcrumb {
   name: string;
+  children?: ReactNode;
   href?: string;
 }
 
@@ -23,35 +31,42 @@ export default function Breadcrumbs({
   const rest = items.slice(1);
 
   return (
-    <Paper
-      sx={{
-        background: "transparent",
-        borderBottom: `1px solid ${
-          theme.colorScheme === "dark"
-            ? theme.colors.dark[6]
-            : theme.colors.gray[3]
-        }`,
-        display: "flex",
-        alignItems: "center",
+    <MediaQuery
+      smallerThan="xs"
+      styles={{
+        display: "none",
       }}
-      radius={0}
     >
-      <Box
-        component="ol"
-        role="list"
-        sx={{ display: "flex", margin: 0, padding: 0 }}
+      <Paper
+        sx={{
+          background: "transparent",
+          borderBottom: `1px solid ${
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[6]
+              : theme.colors.gray[3]
+          }`,
+          display: "flex",
+          alignItems: "center",
+        }}
+        radius={0}
       >
         <Box
-          component="li"
-          sx={{
-            listStyleType: "none",
-            display: "flex",
-            alignItems: "center",
-            marginLeft: "1rem",
-          }}
+          component="ol"
+          role="list"
+          sx={{ display: "flex", margin: 0, padding: 0 }}
         >
-          <NextLink href={first.href || ""} passHref>
+          <Box
+            component="li"
+            sx={{
+              listStyleType: "none",
+              display: "flex",
+              alignItems: "center",
+              marginLeft: "1rem",
+            }}
+          >
             <Anchor
+              component={NextLink}
+              href={first.href || ""}
               variant="text"
               sx={{
                 display: "flex",
@@ -69,64 +84,66 @@ export default function Breadcrumbs({
             >
               <IconHome size={20} aria-label={first.name} />
             </Anchor>
-          </NextLink>
-        </Box>
-        {rest.map((item) => (
-          <Box
-            component="li"
-            sx={{
-              listStyleType: "none",
-              display: "flex",
-              alignItems: "center",
-              marginLeft: "1rem",
-            }}
-            key={item.name}
-          >
+          </Box>
+          {rest.map((item) => (
             <Box
-              component="svg"
+              component="li"
               sx={{
-                height: "100%",
-                width: "1.5rem",
-                color:
-                  theme.colorScheme === "dark"
-                    ? theme.colors.dark[5]
-                    : theme.colors.gray[3],
-                marginRight: "1rem",
+                listStyleType: "none",
+                display: "flex",
+                alignItems: "center",
+                marginLeft: "1rem",
               }}
-              className="flex-shrink-0 w-6 h-full text-slate-200 dark:text-slate-800"
-              viewBox="0 0 24 44"
-              preserveAspectRatio="none"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
+              key={item.name}
             >
-              <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-            </Box>
-            {item.href ? (
-              <NextLink href={item.href} passHref>
+              <Box
+                component="svg"
+                sx={{
+                  height: "100%",
+                  width: "1.5rem",
+                  color:
+                    theme.colorScheme === "dark"
+                      ? theme.colors.dark[5]
+                      : theme.colors.gray[3],
+                  marginRight: "1rem",
+                }}
+                className="flex-shrink-0 w-6 h-full text-slate-200 dark:text-slate-800"
+                viewBox="0 0 24 44"
+                preserveAspectRatio="none"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+              >
+                <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+              </Box>
+              {item.children ? (
+                item.children
+              ) : item.href ? (
                 <Anchor
+                  component={NextLink}
+                  href={item.href}
                   size="sm"
                   aria-current={route === item.href ? "page" : undefined}
                   color={theme.colorScheme === "dark" ? "gray.5" : "gray.7"}
                 >
                   {item.name}
                 </Anchor>
-              </NextLink>
-            ) : (
-              <Text
-                size="sm"
-                weight={500}
-                color={theme.colorScheme === "dark" ? "gray.5" : "gray.7"}
-              >
-                {item.name}
-              </Text>
-            )}
-          </Box>
-        ))}
-      </Box>
-      {action && (
-        <Box sx={{ marginLeft: "auto", marginRight: "1rem" }}>{action}</Box>
-      )}
-    </Paper>
+              ) : (
+                <Text
+                  size="sm"
+                  weight={500}
+                  color={theme.colorScheme === "dark" ? "gray.5" : "gray.7"}
+                >
+                  {item.name}
+                </Text>
+              )}
+            </Box>
+          ))}
+        </Box>
+        {action && (
+          <Box sx={{ marginLeft: "auto", marginRight: "1rem" }}>{action}</Box>
+        )}
+      </Paper>
+    </MediaQuery>
   );
 }
