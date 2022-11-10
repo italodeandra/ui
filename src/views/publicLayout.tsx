8f9e7b8e999ca67aa0ada00b10f72a1ce80c7e4b
Ítalo Andrade <italodeandra@gmail.com>
2022-11-10 17:52:17 -0300

    fix components location

--- src/views/publicLayout.tsx
+++ src/views/publicLayout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
-import BottomBlurryPoint from "../pages/BackgroundEffects/BottomBlurryPoint";
-import TopBlurryPoint from "../pages/BackgroundEffects/TopBlurryPoint";
+import BottomBlurryPoint from "../../lib/components/BackgroundEffects/BottomBlurryPoint";
+import TopBlurryPoint from "../../lib/components/BackgroundEffects/TopBlurryPoint";
 import Header from "./Header/Header";
 import NavigationDrawer from "./Header/NavigationDrawer";
 
