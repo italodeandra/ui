644d85e7787da4f46aec4fd10e8ee806601d6b3f
Ítalo Andrade <italodeandra@gmail.com>
2022-11-13 18:36:06 -0300

    add home link to logo

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -3,6 +3,7 @@ import UiHeader from "../../../lib/components/Header/Header";
 import { Bars3BottomLeftIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import navigationDrawerState from "../../../lib/components/NavigationDrawer/navigationDrawer.state";
+import NextLink from "next/link";
 
 export default function Header() {
   return (
@@ -15,13 +16,15 @@ export default function Header() {
       >
         <Bars3BottomLeftIcon />
       </Button>
-      <Image
-        src="/favicon.ico"
-        width={34}
-        height={34}
-        alt="Logo"
-        className="ml-3"
-      />
+      <NextLink href="">
+        <Image
+          src="/favicon.ico"
+          width={34}
+          height={34}
+          alt="Logo"
+          className="ml-3"
+        />
+      </NextLink>
       <span className="ml-2 text-xl font-medium">@italodeandra/ui</span>
     </UiHeader>
   );
