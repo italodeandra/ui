6d0ca45cc9eeae6f9aacf857c64a0351a12e6cc5
Ítalo Andrade <italodeandra@gmail.com>
2022-12-25 17:19:52 -0300

    remove height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => ({
 
 export default function HomePage() {
   return (
-    <div className="mx-auto h-[2000px] max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-7xl py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
       <div className="text-center">
         <h2 className="text-lg font-semibold text-primary-600">Demo</h2>
         <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl lg:text-6xl">
