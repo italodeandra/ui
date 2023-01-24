2eedae4a264fe8d99a277bab470c10a77202af29
Ítalo Andrade <italodeandra@gmail.com>
2023-01-24 00:13:20 -0300

    accept any

--- lib/bootstrap/AppProps.ts
+++ lib/bootstrap/AppProps.ts
@@ -3,6 +3,7 @@ import { NextComponentType, NextPageContext } from "next/dist/shared/lib/utils";
 import { ReactNode } from "react";
 
 type AppProps = Omit<NAppProps, "Component"> & {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   Component: NextComponentType<NextPageContext, any, any> & {
     getLayout: (children: ReactNode) => ReactNode;
   };
