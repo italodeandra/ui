83b367beec1cf83654dcfd288b685201ee5a6262
Ítalo Andrade <italodeandra@gmail.com>
2023-02-03 15:10:36 -0300

    fix color

--- lib/components/Modal/Modal.tsx
+++ lib/components/Modal/Modal.tsx
@@ -32,7 +32,7 @@ export default function Modal({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 !bg-opacity-75 transition-opacity dark:bg-zinc-800" />
+          <div className="fixed inset-0 bg-zinc-500 !bg-opacity-75 transition-opacity dark:bg-zinc-800" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
