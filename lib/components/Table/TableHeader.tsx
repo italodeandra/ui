67b901e11e5c5a753c55b1bc3261affdcb58ac85
Ítalo Andrade <italodeandra@gmail.com>
2023-03-03 11:37:03 -0300

    fix when divs should be rendered

--- lib/components/Table/TableHeader.tsx
+++ lib/components/Table/TableHeader.tsx
@@ -14,19 +14,24 @@ export default function TableHeader({
   return (
     <div className="px-4 md:px-0">
       <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          {title && (
-            <h1 className="text-xl font-semibold text-gray-900 dark:text-white">
-              {title}
-            </h1>
-          )}
-          {subtitle && (
-            <p className="mt-2 text-sm text-zinc-700 dark:text-zinc-300">
-              {subtitle}
-            </p>
-          )}
-        </div>
-        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">{children}</div>
+        {title ||
+          (subtitle && (
+            <div className="sm:flex-auto">
+              {title && (
+                <h1 className="text-xl font-semibold text-gray-900 dark:text-white">
+                  {title}
+                </h1>
+              )}
+              {subtitle && (
+                <p className="mt-2 text-sm text-zinc-700 dark:text-zinc-300">
+                  {subtitle}
+                </p>
+              )}
+            </div>
+          ))}
+        {children && (
+          <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">{children}</div>
+        )}
       </div>
     </div>
   );
