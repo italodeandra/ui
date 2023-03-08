8ba1a9f060c72451e4eb97d53016fd1f3c164bc6
Ítalo Andrade <italodeandra@gmail.com>
2023-03-08 15:12:35 -0300

    move `useModeToggle` to its own file

--- lib/components/ModeToggle/ModeToggle.tsx
+++ lib/components/ModeToggle/ModeToggle.tsx
@@ -2,36 +2,12 @@ import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import Button from "../Button/Button";
 import clsx from "clsx";
 import { ForwardedRef, forwardRef } from "react";
+import { useModeToggle } from "./useModeToggle";
 
 export interface ModeToggleProps {
   ariaLabel?: string;
   className?: string;
   iconClassName?: string;
-}
-
-export function useModeToggle() {
-  function disableTransitionsTemporarily() {
-    document.documentElement.classList.add("[&_*]:!transition-none");
-    window.setTimeout(() => {
-      document.documentElement.classList.remove("[&_*]:!transition-none");
-    }, 0);
-  }
-
-  function toggleMode() {
-    disableTransitionsTemporarily();
-
-    let darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-    let isSystemDarkMode = darkModeMediaQuery.matches;
-    let isDarkMode = document.documentElement.classList.toggle("dark");
-
-    if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode;
-    } else {
-      window.localStorage.isDarkMode = isDarkMode;
-    }
-  }
-
-  return toggleMode;
 }
 
 const ModeToggle = forwardRef(function ModeToggle(
---
+++ lib/components/ModeToggle/useModeToggle.tsx
@@ -0,0 +1,24 @@
+export function useModeToggle() {
+  function disableTransitionsTemporarily() {
+    document.documentElement.classList.add("[&_*]:!transition-none");
+    window.setTimeout(() => {
+      document.documentElement.classList.remove("[&_*]:!transition-none");
+    }, 0);
+  }
+
+  function toggleMode() {
+    disableTransitionsTemporarily();
+
+    let darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    let isSystemDarkMode = darkModeMediaQuery.matches;
+    let isDarkMode = document.documentElement.classList.toggle("dark");
+
+    if (isDarkMode === isSystemDarkMode) {
+      delete window.localStorage.isDarkMode;
+    } else {
+      window.localStorage.isDarkMode = isDarkMode;
+    }
+  }
+
+  return toggleMode;
+}
