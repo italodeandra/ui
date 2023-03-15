738f118bce1cbeb5ab556fc1bfbc7fa275749b4a
Ítalo Andrade <italodeandra@gmail.com>
2023-03-15 01:50:39 -0300

    fix title and subtitle

--- lib/components/Table/TableHeader.tsx
+++ lib/components/Table/TableHeader.tsx
@@ -1,4 +1,6 @@
 import { ReactNode } from "react";
+import Text from "../Text/Text";
+import Stack from "../Stack/Stack";
 
 export type TableHeaderProps = {
   title?: ReactNode;
@@ -14,21 +16,16 @@ export default function TableHeader({
   return (
     <div className="px-4 md:px-0">
       <div className="sm:flex sm:items-center">
-        {title ||
-          (subtitle && (
-            <div className="sm:flex-auto">
-              {title && (
-                <h1 className="text-xl font-semibold text-gray-900 dark:text-white">
-                  {title}
-                </h1>
-              )}
-              {subtitle && (
-                <p className="mt-2 text-sm text-zinc-700 dark:text-zinc-300">
-                  {subtitle}
-                </p>
-              )}
-            </div>
-          ))}
+        {(title || subtitle) && (
+          <Stack className="sm:flex-auto">
+            {title && (
+              <Text variant="label" size="lg">
+                {title}
+              </Text>
+            )}
+            {subtitle && <Text variant="secondary">{subtitle}</Text>}
+          </Stack>
+        )}
         {children && (
           <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">{children}</div>
         )}
