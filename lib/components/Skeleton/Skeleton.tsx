14e7d8cdafd3179972dc0af9096cbf82ff98c7a0
Ítalo Andrade <italodeandra@gmail.com>
2024-01-24 14:29:56 -0300

    hide text

--- lib/components/Skeleton/Skeleton.tsx
+++ lib/components/Skeleton/Skeleton.tsx
@@ -10,7 +10,8 @@ export default function Skeleton({
   return (
     <div
       className={clsx(
-        "animate-pulse rounded bg-zinc-300 dark:bg-zinc-700",
+        "animate-pulse rounded",
+        "bg-zinc-300 text-zinc-300 dark:bg-zinc-700 dark:text-zinc-700",
         className,
       )}
       {...props}
