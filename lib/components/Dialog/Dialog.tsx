d9ec2b589840ee78efa0fa763ad1f05ba9b3d418
Ítalo Andrade <italodeandra@gmail.com>
2024-01-30 09:18:57 -0300

    fix dialog center

--- lib/components/Dialog/Dialog.tsx
+++ lib/components/Dialog/Dialog.tsx
@@ -25,54 +25,54 @@ export default function Dialog({
         <RDialog.Overlay
           className={clsx(
             "ui-dialog-overlay",
-            "z-20 bg-black/50 fixed inset-0",
-            "data-[state=open]:animate-fadeIn data-[state=closed]:animate-fadeOut",
-          )}
-        />
-        <RDialog.Content
-          className={clsx(
-            modalContentClassName,
-            "ui-dialog-content",
-            "p-4 fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] focus:outline-none flex flex-col gap-3",
-            "data-[state=open]:animate-dialogContentShow data-[state=closed]:animate-fadeOut",
+            "z-20 bg-black/50 fixed inset-0 flex items-center justify-center",
+            "data-[state=open]:animate-slideUpAndFade data-[state=closed]:animate-fadeOut will-change-[opacity,transform]",
           )}
         >
-          {title && (
-            <RDialog.Title
-              className={clsx(
-                "ui-dialog-title",
-                "text-zinc-900 dark:text-zinc-50 text-lg font-medium -mb-1",
-              )}
-            >
-              {title}
-            </RDialog.Title>
-          )}
-          {description && (
-            <RDialog.Description
-              className={clsx(
-                "ui-dialog-description",
-                "text-zinc-700 dark:text-zinc-300",
-              )}
-            >
-              {description}
-            </RDialog.Description>
-          )}
-          {children}
-          <RDialog.Close asChild>
-            <Button
-              className={clsx(
-                "ui-dialog-close-button",
-                "absolute top-1 right-1",
-              )}
-              aria-label="Close"
-              variant="text"
-              icon
-              size="sm"
-            >
-              <XMarkIcon />
-            </Button>
-          </RDialog.Close>
-        </RDialog.Content>
+          <RDialog.Content
+            className={clsx(
+              modalContentClassName,
+              "ui-dialog-content",
+              "p-4 max-h-[85vh] w-[90vw] max-w-[450px] focus:outline-none flex flex-col gap-3",
+            )}
+          >
+            {title && (
+              <RDialog.Title
+                className={clsx(
+                  "ui-dialog-title",
+                  "text-zinc-900 dark:text-zinc-50 text-lg font-medium -mb-1",
+                )}
+              >
+                {title}
+              </RDialog.Title>
+            )}
+            {description && (
+              <RDialog.Description
+                className={clsx(
+                  "ui-dialog-description",
+                  "text-zinc-700 dark:text-zinc-300",
+                )}
+              >
+                {description}
+              </RDialog.Description>
+            )}
+            {children}
+            <RDialog.Close asChild>
+              <Button
+                className={clsx(
+                  "ui-dialog-close-button",
+                  "absolute top-1 right-1",
+                )}
+                aria-label="Close"
+                variant="text"
+                icon
+                size="sm"
+              >
+                <XMarkIcon />
+              </Button>
+            </RDialog.Close>
+          </RDialog.Content>
+        </RDialog.Overlay>
       </RDialog.Portal>
     </RDialog.Root>
   );
