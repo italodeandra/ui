9c1fbdb37fb441dc12edd5ef909db805d5f74419
Ítalo Andrade <italodeandra@gmail.com>
2024-02-04 19:06:14 -0300

    fix date input

--- lib/components/DateInput/DateInput.tsx
+++ lib/components/DateInput/DateInput.tsx
@@ -1,4 +1,4 @@
-import { ForwardedRef, forwardRef } from "react";
+import { ForwardedRef, forwardRef, useMemo } from "react";
 import Input, { InputProps } from "../Input";
 import { CalendarIcon } from "@heroicons/react/20/solid";
 import { useRefValue } from "./useRefValue";
@@ -11,26 +11,43 @@ function DateInput(
     onValueChange,
     ...props
   }: Omit<InputProps<false>, "value" | "crossOrigin"> & {
-    value?: Date | string;
+    value?: string;
     onValueChange?: (value?: string) => void;
   },
   ref: ForwardedRef<HTMLInputElement>,
 ) {
   const realRef = useRefValue(ref);
 
+  const formattedValue = useMemo(
+    () => (value && value.includes("T") ? value.split("T")[0] : value) || "",
+    [value],
+  );
+
   return (
     <Input
       {...props}
-      onChange={(e) => onValueChange?.(e.target.value || undefined)}
+      onChange={(e) =>
+        onValueChange?.(
+          e.target.value
+            ? `${e.target.value}T${
+                dayjs()
+                  .set("hour", 0)
+                  .set("minute", 0)
+                  .set("second", 0)
+                  .set("millisecond", 0)
+                  .toISOString()
+                  .split("T")[1]
+              }`
+            : e.target.value,
+        )
+      }
       ref={realRef}
       type="date"
       pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}"
       trailing={!readOnly ? <CalendarIcon className="w-5" /> : undefined}
       inputClassName="!pr-3"
       readOnly={readOnly}
-      value={
-        typeof value === "object" ? dayjs(value).format("YYYY-MM-DD") : value
-      }
+      value={formattedValue}
     />
   );
 }
--- src/pages/date-input.tsx
+++ src/pages/date-input.tsx
@@ -5,6 +5,8 @@ import Breadcrumbs from "../../lib/components/Breadcrumbs";
 import Stack from "../../lib/components/Stack";
 import getPublicLayout from "../views/publicLayout";
 import DateInput, { DateTimeInput } from "../../lib/components/DateInput";
+import { useState } from "react";
+import dayjs from "dayjs";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => ({
   props: {
@@ -15,12 +17,16 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => ({
 const pages = [{ title: "Date time input" }];
 
 export default function Page() {
+  let [date, setDate] = useState<string | undefined>(
+    dayjs("2024-01-01").toISOString(),
+  );
+
   return (
     <>
       <NextSeo title={pages[0].title} />
       <Breadcrumbs pages={pages} className="mb-2 md:mx-2" />
       <Stack className="p-2">
-        <DateInput label="Birth date" onValueChange={console.info} />
+        <DateInput label="Birth date" value={date} onValueChange={setDate} />
         <DateTimeInput label="Event date" />
       </Stack>
     </>
