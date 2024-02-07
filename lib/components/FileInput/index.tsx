c2c3d72845a00457a41d2291efaebfebb9a00047
Ítalo Andrade <italodeandra@gmail.com>
2024-02-07 08:45:00 -0300

    add support to webp

--- lib/components/FileInput/index.tsx
+++ lib/components/FileInput/index.tsx
@@ -3,6 +3,3 @@ import FileInput from "./FileInput";
 export * from "./FileInput";
 
 export default FileInput;
-export { PreviewFile } from "./PreviewFile";
-export { imageExtensions } from "./PreviewFile";
-export { videoExtensions } from "./PreviewFile";
