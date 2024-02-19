56dfd80e939031bef5eff4a380252eb1926c7186
Ítalo Andrade <italodeandra@gmail.com>
2024-02-19 17:08:20 -0300

    move class names for the day picker to styles folder

--- lib/components/DateRangePicker/DateRangePicker.tsx
+++ lib/components/DateRangePicker/DateRangePicker.tsx
@@ -12,58 +12,12 @@ import Button from "../Button";
 import clsx from "../../utils/clsx";
 import { useDeepCompareEffect } from "react-use";
 import Popover from "../Popover";
+import {
+  dayPickerButtonClassName,
+  dayPickerClassNames,
+} from "../../styles/DayPicker.classNames";
 
 export type { DateRange };
-
-const classNames = {
-  button: "justify-start pl-4 min-h-[38px]",
-  months: "flex gap-2",
-  caption: "text-center h-9 flex items-center justify-center mx-9",
-  navButtonPrevious: "left-2 top-2",
-  navButtonNext: "right-2 top-2",
-  navButton: clsx(
-    "absolute border rounded p-1.5 [&_svg]:w-3 [&_svg]:h-3 transition",
-    "border-zinc-100 hover:bg-zinc-100 bg-white active:border-zinc-200",
-    "dark:border-zinc-800 dark:hover:bg-zinc-800 dark:bg-zinc-900 dark:active:border-zinc-700",
-    "disabled:opacity-50 disabled:cursor-not-allowed",
-  ),
-  head: "text-xs dark:text-zinc-500",
-  headCell: "font-normal",
-  day: clsx(
-    "w-8 h-8 rounded transition",
-    "text-zinc-700 hover:bg-black/10",
-    "dark:text-zinc-300 dark:hover:bg-white/10",
-    "disabled:opacity-30 disabled:cursor-not-allowed",
-    "dark:disabled:opacity-20",
-  ),
-  dayRangeStart: clsx(
-    "!text-onPrimary dark:!text-onPrimary",
-    "bg-primary-500 hover:bg-primary-500/50",
-    "dark:bg-primary-600 dark:hover:bg-primary-600/50",
-  ),
-  dayRangeEnd: clsx(
-    "!text-onPrimary dark:!text-onPrimary",
-    "bg-primary-500 hover:bg-primary-500/50",
-    "dark:bg-primary-600 dark:hover:bg-primary-600/50",
-  ),
-  table: "border-spacing-y-1 border-separate",
-  cell: clsx(
-    "p-0",
-    "has-[.ui-date-picker-day-outside]:opacity-40",
-    "dark:has-[.ui-date-picker-day-outside]:opacity-30",
-
-    "has-[.ui-date-picker-day-range-start]:rounded-l first:rounded-l",
-    "has-[.ui-date-picker-day-range-start]:bg-black/5",
-    "dark:has-[.ui-date-picker-day-range-start]:bg-white/10",
-
-    "has-[.ui-date-picker-day-range-middle]:bg-black/5",
-    "dark:has-[.ui-date-picker-day-range-middle]:bg-white/10",
-
-    "has-[.ui-date-picker-day-range-end]:rounded-r last:rounded-r",
-    "has-[.ui-date-picker-day-range-end]:bg-black/5",
-    "dark:has-[.ui-date-picker-day-range-end]:bg-white/10",
-  ),
-};
 
 export default function DateRangePicker({
   value,
@@ -114,7 +68,7 @@ export default function DateRangePicker({
     <Button
       {...buttonProps}
       leading={buttonProps?.leading || <CalendarIcon />}
-      className={clsx(classNames.button, buttonProps?.className)}
+      className={clsx(dayPickerButtonClassName, buttonProps?.className)}
     >
       {buttonText}
     </Button>
@@ -131,30 +85,7 @@ export default function DateRangePicker({
           onSelect={setRange}
           numberOfMonths={2}
           showOutsideDays
-          classNames={{
-            months: classNames.months,
-            caption: classNames.caption,
-            nav_button_previous: classNames.navButtonPrevious,
-            nav_button_next: classNames.navButtonNext,
-            nav_button: classNames.navButton,
-            head: classNames.head,
-            head_cell: classNames.headCell,
-            day: classNames.day,
-            cell: classNames.cell,
-            table: classNames.table,
-
-            day_range_start: clsx(
-              "ui-date-picker-day-range-start",
-              classNames.dayRangeStart,
-            ),
-            day_range_end: clsx(
-              "ui-date-picker-day-range-end",
-              classNames.dayRangeEnd,
-            ),
-
-            day_outside: "ui-date-picker-day-outside",
-            day_range_middle: "ui-date-picker-day-range-middle",
-          }}
+          classNames={dayPickerClassNames}
           fromDate={fromDate}
           toDate={toDate}
           min={min}
---
+++ lib/styles/DayPicker.classNames.ts
@@ -0,0 +1,72 @@
+import clsx from "../utils/clsx";
+
+export const dayPickerButtonClassName = "justify-start pl-4 min-h-[38px]";
+export const dayPickerMonthsClassName = "flex gap-2";
+export const dayPickerCaptionClassName =
+  "text-center h-9 flex items-center justify-center mx-9";
+export const dayPickerNavButtonPreviousClassName = "left-2 top-2";
+export const dayPickerNavButtonNextClassName = "right-2 top-2";
+export const dayPickerNavButtonClassName = clsx(
+  "absolute border rounded p-1.5 [&_svg]:w-3 [&_svg]:h-3 transition",
+  "border-zinc-100 hover:bg-zinc-100 bg-white active:border-zinc-200",
+  "dark:border-zinc-800 dark:hover:bg-zinc-800 dark:bg-zinc-900 dark:active:border-zinc-700",
+  "disabled:opacity-50 disabled:cursor-not-allowed",
+);
+export const dayPickerHeadClassName = "text-xs dark:text-zinc-500";
+export const dayPickerHeadCellClassName = "font-normal";
+export const dayPickerDayClassName = clsx(
+  "w-8 h-8 rounded transition",
+  "text-zinc-700 hover:bg-black/10",
+  "dark:text-zinc-300 dark:hover:bg-white/10",
+  "disabled:opacity-30 disabled:cursor-not-allowed",
+  "dark:disabled:opacity-20",
+);
+export const dayPickerDayRangeStartClassName = clsx(
+  "ui-date-picker-day-range-start",
+  "!text-onPrimary dark:!text-onPrimary",
+  "bg-primary-500 hover:bg-primary-500/50",
+  "dark:bg-primary-600 dark:hover:bg-primary-600/50",
+);
+export const dayPickerDayRangeEndClassName = clsx(
+  "ui-date-picker-day-range-end",
+  "!text-onPrimary dark:!text-onPrimary",
+  "bg-primary-500 hover:bg-primary-500/50",
+  "dark:bg-primary-600 dark:hover:bg-primary-600/50",
+);
+export const dayPickerTableClassName = "border-spacing-y-1 border-separate";
+export const dayPickerCellClassName = clsx(
+  "p-0",
+  "has-[.ui-date-picker-day-outside]:opacity-40",
+  "dark:has-[.ui-date-picker-day-outside]:opacity-30",
+
+  "has-[.ui-date-picker-day-range-start]:rounded-l first:rounded-l",
+  "has-[.ui-date-picker-day-range-start]:bg-black/5",
+  "dark:has-[.ui-date-picker-day-range-start]:bg-white/10",
+
+  "has-[.ui-date-picker-day-range-middle]:bg-black/5",
+  "dark:has-[.ui-date-picker-day-range-middle]:bg-white/10",
+
+  "has-[.ui-date-picker-day-range-end]:rounded-r last:rounded-r",
+  "has-[.ui-date-picker-day-range-end]:bg-black/5",
+  "dark:has-[.ui-date-picker-day-range-end]:bg-white/10",
+);
+export const dayPickerDayOutsideClassName = "ui-date-picker-day-outside";
+export const dayPickerDayRangeMiddleClassName =
+  "ui-date-picker-day-range-middle";
+
+export const dayPickerClassNames = {
+  months: dayPickerMonthsClassName,
+  caption: dayPickerCaptionClassName,
+  nav_button_previous: dayPickerNavButtonPreviousClassName,
+  nav_button_next: dayPickerNavButtonNextClassName,
+  nav_button: dayPickerNavButtonClassName,
+  head: dayPickerHeadClassName,
+  head_cell: dayPickerHeadCellClassName,
+  day: dayPickerDayClassName,
+  cell: dayPickerCellClassName,
+  table: dayPickerTableClassName,
+  day_range_start: dayPickerDayRangeStartClassName,
+  day_range_end: dayPickerDayRangeEndClassName,
+  day_outside: dayPickerDayOutsideClassName,
+  day_range_middle: dayPickerDayRangeMiddleClassName,
+};
