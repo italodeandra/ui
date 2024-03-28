6c4de57461fc761028c558c5206c6c89f877fbce
Ítalo Andrade <italodeandra@gmail.com>
2024-03-28 16:30:06 -0300

    fix class

--- lib/components/Table/TableCell.tsx
+++ lib/components/Table/TableCell.tsx
@@ -20,7 +20,6 @@ export default function TableCell({
 
   const commonClassName = clsx(
     "text-sm first:pl-4 first:sm:pl-6 first:font-medium first:text-zinc-900 dark:first:text-zinc-100 px-3",
-    className,
     {
       "py-2 last:pr-4 last:sm:pr-6": !actions,
       "py-0.5 text-right pr-2.5 sm:pr-5": actions,
@@ -33,6 +32,7 @@ export default function TableCell({
         className={clsx(
           commonClassName,
           "sticky left-0 bg-zinc-50 py-2 text-left font-semibold text-zinc-900 dark:bg-zinc-900 dark:text-zinc-50",
+          className,
         )}
         colSpan={colSpan}
         title={title}
@@ -47,6 +47,7 @@ export default function TableCell({
       className={clsx(
         commonClassName,
         "whitespace-nowrap text-zinc-500 dark:text-zinc-300",
+        className,
       )}
       colSpan={colSpan}
       title={title}
