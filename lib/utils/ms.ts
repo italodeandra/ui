d914118904b833589d24695ad7d1ab4665e7bff8
Ítalo Andrade <italodeandra@gmail.com>
2024-06-17 22:20:51 -0300

    fix locale

--- lib/utils/ms.ts
+++ lib/utils/ms.ts
@@ -2,40 +2,40 @@ import vms from "ms";
 
 const locale: Record<string, Record<string, string>> = {
   ["en"]: {
-    days: "days",
-    hours: "hours",
-    minutes: "minutes",
-    seconds: "seconds",
+    day: "day",
+    hour: "hour",
+    minute: "minute",
+    second: "second",
   },
   ["pt-BR"]: {
-    days: "dias",
-    hours: "horas",
-    minutes: "minutos",
-    seconds: "segundos",
+    day: "dia",
+    hour: "hora",
+    minute: "minuto",
+    second: "segundo",
   },
   ["undefined"]: {
-    days: "days",
-    hours: "hours",
-    seconds: "seconds",
-    minutes: "minutes",
+    day: "day",
+    hour: "hour",
+    second: "second",
+    minute: "minute",
   },
 };
 
 export function ms(
   value: number,
-  options?: { long: boolean; locale?: string }
+  options?: { long: boolean; locale?: string },
 ): string;
 export function ms(value: string): number;
 export function ms(
   value: number | string,
-  options?: { long: boolean; locale?: string }
+  options?: { long: boolean; locale?: string },
 ) {
   if (typeof value === "number") {
     return vms(value, options)
-      .replace("days", locale[String(options?.locale)].days)
-      .replace("hours", locale[String(options?.locale)].hours)
-      .replace("minutes", locale[String(options?.locale)].minutes)
-      .replace("seconds", locale[String(options?.locale)].seconds);
+      .replace("day", locale[String(options?.locale)].day)
+      .replace("hour", locale[String(options?.locale)].hour)
+      .replace("minute", locale[String(options?.locale)].minute)
+      .replace("second", locale[String(options?.locale)].second);
   }
   return vms(value);
 }
