8afc9ec60b83b0abe0cd58384671fa6879c43031
Ítalo Andrade <italodeandra@gmail.com>
2024-06-17 22:20:19 -0300

    improve render

--- lib/components/NavigationDrawer/NavigationDrawer.tsx
+++ lib/components/NavigationDrawer/NavigationDrawer.tsx
@@ -1,8 +1,7 @@
 import Drawer from "../Drawer";
-import { ReactNode } from "react";
+import { ReactNode, useCallback } from "react";
 import navigationDrawerState from "./navigationDrawer.state";
 import { useSnapshot } from "valtio";
-import { Transition } from "@headlessui/react";
 import clsx from "../../utils/clsx";
 import defaultTheme from "tailwindcss/defaultTheme";
 
@@ -23,15 +22,17 @@ export default function NavigationDrawer({
 }) {
   const { isOpen, setOpen } = useSnapshot(navigationDrawerState);
 
+  const handleChangeOpen = useCallback((open: boolean) => {
+    if (window.innerWidth <= +defaultTheme.screens.lg.replace("px", "")) {
+      navigationDrawerState.setOpen(open);
+    }
+  }, []);
+
   return (
     <>
       <Drawer
         open={isOpen}
-        onChangeOpen={(open) => {
-          if (window.innerWidth <= +defaultTheme.screens.sm.replace("px", "")) {
-            setOpen(open);
-          }
-        }}
+        onChangeOpen={handleChangeOpen}
         className="lg:hidden"
         position={position}
         title={title}
@@ -40,23 +41,21 @@ export default function NavigationDrawer({
       >
         {navigationChildren}
       </Drawer>
-      <Transition
+      <div
         className={clsx(
           "fixed left-0 top-0 z-10 hidden h-full w-full max-w-xs flex-1 overflow-y-auto px-4 py-4 pt-20 lg:block",
           "bg-zinc-100/95 backdrop-blur [@supports(backdrop-filter:blur(0))]:bg-zinc-100/75",
           "dark:bg-zinc-900/95 dark:[@supports(backdrop-filter:blur(0))]:bg-zinc-900/75",
+          "transition-transform",
+          {
+            "translate-x-0": isOpen,
+            "-translate-x-full": !isOpen,
+          },
           panelClassName,
         )}
-        show={isOpen}
-        enter="transition-transform duration-150"
-        enterFrom="-translate-x-full"
-        enterTo="translate-x-0"
-        leave="transition-transform duration-300"
-        leaveFrom="translate-x-0"
-        leaveTo="-translate-x-full"
       >
         {navigationChildren}
-      </Transition>
+      </div>
       <div
         className={clsx(
           "flex min-h-screen flex-col ring-offset-zinc-100 transition-all duration-300",
