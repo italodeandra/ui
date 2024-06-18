1d63b1d5e415b4822a4de68a5dddbf1216f6220b
Ítalo Andrade <italodeandra@gmail.com>
2024-06-17 22:19:58 -0300

    fix custom classname

--- lib/components/Table/TableRow.tsx
+++ lib/components/Table/TableRow.tsx
@@ -4,18 +4,22 @@ import { ForwardedRef, forwardRef, MouseEventHandler, ReactNode } from "react";
 export type TableRowProps = {
   children?: ReactNode;
   onClick?: MouseEventHandler<HTMLTableRowElement>;
+  className?: string;
 };
 function TableRow(
-  { children, onClick, ...props }: TableRowProps,
+  { children, onClick, className, ...props }: TableRowProps,
   ref: ForwardedRef<HTMLTableRowElement>,
 ) {
   return (
     <tr
       ref={ref}
       onClick={onClick}
-      className={clsx({
-        "cursor-pointer hover:bg-black/5 dark:hover:bg-white/5": !!onClick,
-      })}
+      className={clsx(
+        {
+          "cursor-pointer hover:bg-black/5 dark:hover:bg-white/5": !!onClick,
+        },
+        className,
+      )}
       {...props}
     >
       {children}
