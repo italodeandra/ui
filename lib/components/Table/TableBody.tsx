ee354d9b896de8121cf930fb64692d11ffc1ae31
Ítalo Andrade <italodeandra@gmail.com>
2024-08-25 03:58:26 -0300

    customization

--- lib/components/Table/TableBody.tsx
+++ lib/components/Table/TableBody.tsx
@@ -1,14 +1,20 @@
 import { ReactNode, useMemo } from "react";
 import TableHeadContext from "./TableHeadContext";
+import clsx from "../../utils/clsx";
 
-export type TableBodyProps = { children?: ReactNode };
+export type TableBodyProps = { children?: ReactNode; className?: string };
 
-export default function TableBody({ children }: TableBodyProps) {
+export default function TableBody({ children, className }: TableBodyProps) {
   return (
     <TableHeadContext.Provider
       value={useMemo(() => ({ isHead: false, sticky: false }), [])}
     >
-      <tbody className="divide-y divide-zinc-200 bg-white dark:divide-zinc-700 dark:bg-zinc-800 [&_tr:last-of-type]:!border-b [&_tr:last-of-type]:border-zinc-200 [&_tr:last-of-type]:dark:border-zinc-700">
+      <tbody
+        className={clsx(
+          "divide-y divide-zinc-200 bg-white dark:divide-zinc-700 dark:bg-zinc-800 [&_tr:last-of-type]:border-b [&_tr:last-of-type]:border-zinc-200 [&_tr:last-of-type]:dark:border-zinc-700",
+          className,
+        )}
+      >
         {children}
       </tbody>
     </TableHeadContext.Provider>
