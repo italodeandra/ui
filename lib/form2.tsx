afc5e49e4c547953a6a08e8f84c39992dfc0c509
Ítalo Andrade <italodeandra@gmail.com>
2024-10-08 00:43:43 -0300

    fix type

--- lib/form2.tsx
+++ lib/form2.tsx
@@ -1,44 +1,36 @@
 import {
   Controller as RHFController,
-  useForm as useRHForm,
-} from "react-hook-form";
-import {
+  ControllerFieldState,
   ControllerProps,
+  ControllerRenderProps,
   FieldPath,
   FieldValues,
+  useForm as useRHForm,
   UseFormProps,
   UseFormStateReturn,
-} from "react-hook-form/dist/types";
+} from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import React from "react";
-import {
-  ControllerFieldState,
-  ControllerRenderProps,
-} from "react-hook-form/dist/types/controller"; // eslint-disable-next-line @typescript-eslint/no-explicit-any
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type Any = any;
 
 export function useForm<
-  TFieldValues extends FieldValues = FieldValues,
-  TContext = Any,
->(
-  props?: UseFormProps<TFieldValues, TContext> & {
-    schema: z.Schema<TFieldValues, Any>;
-  },
-) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  TSchema extends z.Schema<any, any>,
+  TFieldValues extends FieldValues = z.infer<TSchema>,
+>({
+  schema,
+  ...props
+}: UseFormProps<TFieldValues> & {
+  schema?: TSchema;
+}) {
   const formReturn = useRHForm({
-    resolver: props?.schema ? zodResolver(props.schema) : undefined,
+    resolver: schema ? zodResolver(schema) : undefined,
     ...props,
   });
   const register = formReturn.register;
   formReturn.register = function (name, options) {
-    const registerReturn = register(name, {
-      ...options,
-      // setValueAs: (value) => (value === "" ? undefined : value),
-    });
-    const zodProperty = (props?.schema as z.ZodObject<Any> | undefined)
+    const registerReturn = register(name, options);
+    const zodProperty = (schema as z.ZodObject<z.infer<TSchema>> | undefined)
       ?.shape?.[name];
     return {
       ...registerReturn,
