fb6aa386342d71aa75400a1065140d9f57945f0d
Ítalo Andrade <italodeandra@gmail.com>
2024-12-06 15:50:31 -0300

    add classname to tooltip

--- lib/components/Tooltip/Tooltip.tsx
+++ lib/components/Tooltip/Tooltip.tsx
@@ -8,11 +8,15 @@ function Tooltip(
     content,
     side,
     delayDuration,
+    className,
+    arrowClassName,
   }: {
     children?: ReactNode;
     content?: ReactNode;
     side?: "top" | "bottom" | "left" | "right";
     delayDuration?: number;
+    className?: string;
+    arrowClassName?: string;
   },
   ref: ForwardedRef<HTMLButtonElement>,
 ) {
@@ -32,12 +36,18 @@ function Tooltip(
               "z-20 rounded bg-zinc-950 px-2 py-1 text-center text-sm text-zinc-50 shadow",
               "dark:bg-zinc-50 dark:text-zinc-950",
               "will-change-[transform,opacity] data-[state=closed]:animate-fadeOut data-[state=delayed-open]:data-[side=bottom]:animate-elasticSlideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-elasticSlideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-elasticSlideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-elasticSlideDownAndFade",
+              className,
             )}
             sideOffset={5}
             side={side}
           >
             {content}
-            <RTooltip.Arrow className="fill-zinc-950 dark:fill-zinc-50" />
+            <RTooltip.Arrow
+              className={clsx(
+                "fill-zinc-950 dark:fill-zinc-50",
+                arrowClassName,
+              )}
+            />
           </RTooltip.Content>
         </RTooltip.Portal>
       </RTooltip.Root>
